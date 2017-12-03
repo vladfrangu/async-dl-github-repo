30bc8dbde0050f67860be9476d748c9cf89ce17a
Frangu Vlad <contact@kingdgrizzle.ml>
2017-12-03 17:56:54 +0200

    Rewrite!

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,7 +1,16 @@
 declare module "async-dl-github-repo" {
 
-	declare function download(repo: string, destination: string, downloadOnly? : boolean = false): Promise<DownloadReturn>;
-	export = download;
+	export default function download(url: string, path: string, zipOnly?: boolean): AsyncDLGithubRepo;
+	export const version: string;
+
+	class AsyncDLGithubRepo {
+		public version: string;
+		public constructor (url: string, path: string, zipOnly?: boolean = false);
+
+		public then(resolver: Function, rejecter?: Function): Promise<DownloadReturn>;
+		public catch(rejecter: Function);
+		public end(callback: (err: Error | RangeError | TypeError, result: DownloadReturn) => void): void;
+	}
 
 	type DownloadReturn = {
 		zipPath? : string;
